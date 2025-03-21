c843ab934d8278841a59d0a2bfde09e15912e122
majdajede <68274760+majdajede@users.noreply.github.com>
2025-03-21 15:28:35 +0100

    Add files via upload
    dokumentace

---
+++ dokumentace/phpdoc/api/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\escape\u0028\u0029",
+            "name": "escape",
+            "summary": "Escapes\u0020special\u0020characters\u0020in\u0020a\u0020string\u0020for\u0020use\u0020in\u0020HTML.",
+            "url": "namespaces/default.html#function_escape"
+        },                {
+            "fqsen": "\\csrf_token\u0028\u0029",
+            "name": "csrf_token",
+            "summary": "Generates\u0020and\u0020returns\u0020a\u0020CSRF\u0020token.",
+            "url": "namespaces/default.html#function_csrf_token"
+        },                {
+            "fqsen": "\\check_csrf_token\u0028\u0029",
+            "name": "check_csrf_token",
+            "summary": "Check\u0020if\u0020the\u0020provided\u0020CSRF\u0020token\u0020matches\u0020the\u0020one\u0020stored\u0020in\u0020the\u0020session.",
+            "url": "namespaces/default.html#function_check_csrf_token"
+        },                {
+            "fqsen": "\\processPhotos\u0028\u0029",
+            "name": "processPhotos",
+            "summary": "Processes\u0020and\u0020saves\u0020uploaded\u0020photos.",
+            "url": "namespaces/default.html#function_processPhotos"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
